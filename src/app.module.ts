374138fb251a342c15dbe91c6318f08cf17aa3b5
Nguyen Tuan Anh <ntanh.fit@gmail.com>
2024-12-22 17:44:29 +0700

    quizzes crud and real time quiz game

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GameEventsModule } from './game-events/game-events.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    QuestionsModule,
+    GameEventsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
